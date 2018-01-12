186d983496a60737b4b8a2f8b8a4f22216502dbc
CalebBeichner <35071058+CalebBeichner@users.noreply.github.com>
2018-01-12 08:54:51 -0500

    test
    test

--- team0.py
+++ team0.py
@@ -1,12 +1,5 @@
-####
-# Each team's file must define four tokens:
-#     team_name: a string
-#     strategy_name: a string
-#     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
-####
 
-team_name = 'Beichner' # Only 10 chars displayed.
+team_name = 'Beichner'
 strategy_name = 'Easy Points'
 strategy_description = 'Start off by colluding then, play what other team played before'
     
@@ -16,15 +9,8 @@ def move(my_history, their_history, my_score, their_score):
     
     Make my move.
     Returns 'c' or 'b'. 
-    '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
+    '''    
     
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
     if len(my_history)==0:
         return 'c'
     elif their_history[-1]=='b':
