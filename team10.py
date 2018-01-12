716fb843be947d9b013ee692ee4c886c87b60d96
RileyOtt <35071092+RileyOtt@users.noreply.github.com>
2018-01-12 09:10:51 -0500

    starting to code strategy
    starting to code

--- team10.py
+++ team10.py
@@ -9,14 +9,15 @@
 team_name = 'Riley Ott' # Only 10 chars displayed.
 strategy_name = 'Best Strategy'
 strategy_description = 'picks random first but will know if the same thing is picked every time'
+
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
-    
     Make my move.
     Returns 'c' or 'b'. 
     '''
+    from random import randint
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
@@ -25,15 +26,55 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
+    strategy='always c'
+    plays=len(my_history)
+    if plays==0:
+        if randint(0,1)==0:
+            return 'c'
+        else:
+            return 'b'
+            
+    if 'b' not in their_history:
+        strategy='always c'
     
-    return 'c'
+    if 'c' not in their_history:
+        strategy='always b'
+        
+    if their_history[-1] != their_history[-2]:
+        strategy='alternate'
+    
+    if len(their_history)==4 and 'cbbc' in their_history:
+        strategy='alternate cbbc'
+    
+    if len(their_history)==4 and 'bccb' in their_history:
+        strategy='alternate bccb'
+    
+    if len(their_history)==4 and 'bbbc'in their_history:
+        strategy='alternate bbbc'
+        
+    if len(their_history)==4 and 'cccb' in their_history:
+        strategy='alternate cccb'
+    
+    if len(their_history)==4 and 'bbcc' in their_history:
+        strategy='alternate bbcc'
+        
+    if len(their_history)==4 and 'ccbb' in their_history:
+        strategy='alternate ccbb'
+    
+    if len(their_history)==6 and 'bbbccc' in their_history:
+        strategy='alternate bbbccc'
+        
+    if len(their_history)==4 and 'cccbbb' in their_history:
+        strategy='alternate cccbbb'
 
-    
+
+
+'''
 def test_move(my_history, their_history, my_score, their_score, result):
-    '''calls move(my_history, their_history, my_score, their_score)
+    calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
     Returns True or False, dpending on whether result was as expected.
-    '''
+    
     real_result = move(my_history, their_history, my_score, their_score)
     if real_result == result:
         return True
@@ -66,3 +107,4 @@ if __name__ == '__main__':
               my_score=0, 
               their_score=0,
               result='b')             
+              '''
