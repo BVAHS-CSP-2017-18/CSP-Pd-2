ae28224098026010c306f338ebfd9c660a824c0e
CalebBeichner <35071058+CalebBeichner@users.noreply.github.com>
2018-01-12 08:47:18 -0500

    Edit of team0
    Edit of team 0 strategy

--- team0.py
+++ team0.py
@@ -7,8 +7,8 @@
 ####
 
 team_name = 'Beichner' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'Easy Points'
+strategy_description = 'Start off by colluding then, play what other team played before'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -25,8 +25,12 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    if len(my_history)==0:
+        return 'c'
+    elif their_history[-1]=='b':
+        return 'b'
+    else:
+        return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
