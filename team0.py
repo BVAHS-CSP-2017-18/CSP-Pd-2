1fb15eb039ffb25967d56faacfe2a95e1fc43dbb
CalebBeichner <35071058+CalebBeichner@users.noreply.github.com>
2018-01-16 10:18:13 -0500

    Team 0 Strategy
    Easy Points

--- team0.py
+++ team0.py
@@ -1,14 +1,7 @@
-####
-# Each team's file must define four tokens:
-#     team_name: a string
-#     strategy_name: a string
-#     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
-####
 
-team_name = 'Beichner' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Beichner'
+strategy_name = 'Easy Points'
+strategy_description = 'Start off by colluding then, play what other team played before'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -16,17 +9,14 @@ def move(my_history, their_history, my_score, their_score):
     
     Make my move.
     Returns 'c' or 'b'. 
-    '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
+    '''    
     
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    if len(my_history)==0:
+        return 'c'
+    elif their_history[-1]=='b':
+        return 'b'
+    else:
+        return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
@@ -65,4 +55,4 @@ if __name__ == '__main__':
               # move('bbb', 'ccc', 0, 0) returns 'b'.
               my_score=0, 
               their_score=0,
-              result='b')             
+              result='b')              
