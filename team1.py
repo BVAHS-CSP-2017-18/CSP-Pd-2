261e70f7d7f4dcceeeeda830488550b1ece23f4e
KevinBaeza <35071060+KevinBaeza@users.noreply.github.com>
2018-01-16 10:26:28 -0500

    This is good strat
    Good strat

--- team1.py
+++ team1.py
@@ -7,27 +7,15 @@
 ####
 
 team_name = 'Moosesauce' # Only 10 chars displayed.
-strategy_name = 'With the Friz, NO WAY'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'Friz Strat'
+strategy_description = 'Just Decide'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
+     # If the other player has betrayed or this is the last half of the game, 
+    if 'b' in their_history or len(their_history)>100: 
+        return 'b'               # Betray.
+    else:
+        return 'c'         # but 90% of the time collude
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
